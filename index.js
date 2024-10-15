838738986e258fbf5d3ec27a5f09c8813b527d67
Karan77788 <karanb.bcs22@rathinam.in>
2024-10-15 11:31:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var sidenav =document.querySelector(".side-navbar")
+sidenav.style.display ="block"
+
+function shownavbar()
+{
+    sidenav.style.display="block"
+}
+function closenavbar()
+{
+    sidenav.style.display="block"
+}
